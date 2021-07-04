9055ce68fcbf7664c999dced7046a0d202efa0d3
guisoares9 <gsoares.silvestre@usp.br>
2021-07-04 14:46:03 -0300

    simple changes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import graph
 import plot
 
-G, points = graph.makeGraph(1000, 3)
+G, points = graph.makeGraph(3000, 3)
 
 for i in range(len(G.nodes)):
     print(G[i])
 
 # print(G[9])
 
-plot.plotXY(G, points)
+plot.plotXY(G, points, label = False)
 # plot.plotGraph(G)
 
 
--- plot.py
+++ plot.py
@@ -9,11 +9,22 @@ def plotGraph(graph):
 
     plt.show()
 
-def plotXY(graph, points):
+def plotXY(graph, points, label = True):
     
     edges = list(graph.edges)
     plt.scatter(points[:,0], points[:,1])
     
+    if label:
+        for i in range(len(points)):
+            x, y = points[i][0], points[i][1]
+            label = "{}".format(i)
+
+            plt.annotate(label, # this is the text
+                        (x,y), # these are the coordinates to position the label
+                        textcoords="offset points", # how to position the text
+                        xytext=(0,5), # distance from text to points (x,y)
+                        ha='center') # horizontal alignment can be left, right or center
+
     for edge in edges:
         x_vec = [points[edge[0]][0], points[edge[1]][0]]
         y_vec = [points[edge[0]][1], points[edge[1]][1]]
